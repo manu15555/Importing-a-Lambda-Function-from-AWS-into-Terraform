be496f57ca1afd05ab00b2ca1d127e036e6eb1a8
manu15555 <cufremanu@gmail.com>
2026-07-27 04:54:49 -0300

    Add AWS Lambda function configuration
    Define an AWS Lambda function with code packaging and logging.

---
+++ lambda.tf
@@ -0,0 +1,24 @@
+import {
+  to = aws_lambda_function.this
+  id = "manually-created-lambda"
+}
+
+data "archive_file" "lambda_code" {
+  type        = "zip"
+  source_file = "${path.root}/build/index.mjs"
+  output_path = "${path.root}/lambda.zip"
+}
+
+resource "aws_lambda_function" "this" {
+  description      = "A basic AWS Lambda Function"
+  filename         = "lambda.zip"
+  function_name    = "manually-created-lambda"
+  handler          = "index.handler"
+  role             = aws_iam_role.lambda_execution_role.arn
+  runtime          = "nodejs22.x"
+  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/manually-created-lambda"
+  }
+}
